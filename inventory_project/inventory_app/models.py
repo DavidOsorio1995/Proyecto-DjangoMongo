7d2081fcf824d5add61810272c9e08910f5ba820
francisco leiva <fcoleoleiva@gmail.com>
2023-07-10 19:59:20 -0400

    agregando un poco de estilo
    agregando boostrap

--- inventory_project/inventory_app/models.py
+++ inventory_project/inventory_app/models.py
@@ -4,6 +4,6 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=0)
     description = models.TextField()
-
+    
     def __str__(self):
         return self.name
