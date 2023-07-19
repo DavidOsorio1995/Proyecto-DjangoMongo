ec2b8c338710b7ccd530545ef300af2911728765
Gonzalo Peña(Gyrefallon) <gonzalo.2800@gmail.com>
2023-07-18 21:09:53 -0400

    Arreglo Models.py -> Proveedor

--- inventory_project/inventory_app/models.py
+++ inventory_project/inventory_app/models.py
@@ -19,6 +19,7 @@ class Category(models.Model):
 
 class Provider(models.Model):
     name = models.CharField(max_length=100)
-
+    address = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
     def __str__(self):
         return self.name
