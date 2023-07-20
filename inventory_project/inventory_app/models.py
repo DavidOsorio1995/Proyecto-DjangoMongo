be45c35eeef79ec2d12e9b6d452de54b51c6f842
Gonzalo Peña(Gyrefallon) <gonzalo.2800@gmail.com>
2023-07-20 18:15:55 -0400

    Correccion

--- inventory_project/inventory_app/models.py
+++ inventory_project/inventory_app/models.py
@@ -21,5 +21,6 @@ class Provider(models.Model):
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
+    
     def __str__(self):
         return self.name
